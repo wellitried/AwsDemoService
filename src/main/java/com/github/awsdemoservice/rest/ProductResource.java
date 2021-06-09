33cd9e6b81ba6b7a37445cb87632a1a9ad90538c
Igor Markov <mrkv.igor@gmail.com>
2021-06-09 20:51:15 +0300

    Added logging and dockerfile.

--- src/main/java/com/github/awsdemoservice/rest/ProductResource.java
+++ src/main/java/com/github/awsdemoservice/rest/ProductResource.java
@@ -1,5 +1,7 @@
 package com.github.awsdemoservice.rest;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -10,13 +12,15 @@ import java.util.TreeMap;
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProductResource {
 
+    private static final Logger LOG = Logger.getLogger(ProductResource.class);
+
     private final TreeMap<Long, ProductDto> products = new TreeMap<>();
 
     @GET
     @Path("/{id}")
     public Response get(@PathParam("id") Long id) {
         ProductDto productDto = products.get(id);
-        System.out.println("GET " + id + ": " + productDto.toString());
+        LOG.info("GET " + id + ": " + productDto.toString());
         return Response.ok(productDto).build();
     }
 
@@ -24,7 +28,7 @@ public class ProductResource {
     public Response create(ProductDto productDto) {
         long maxId = products.isEmpty() ? 0 : products.lastKey();
         productDto.id = ++maxId;
-        System.out.println("POST: " + productDto.toString());
+        LOG.info("POST: " + productDto.toString());
         ProductDto savedProductDto = products.put(productDto.id, productDto);
         return Response.ok(savedProductDto).build();
     }
